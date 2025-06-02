d12547628cef7f9cd0fa1b8f103f14be4499ecfb
foolycooly95 <jackwoodman95@gmail.com>
2025-06-02 20:39:10 +0100

    appear

---
+++ src/anniversary/sf33rd/Source/Game/appear.c
@@ -0,0 +1,110 @@
+#include "common.h"
+
+char Appear_car_stop[2]; // size: 0x2, address: 0x578A80
+char Appear_hv[2]; // size: 0x2, address: 0x578A84
+char Appear_free[2]; // size: 0x2, address: 0x578A88
+char Appear_flag[2]; // size: 0x2, address: 0x578A8C
+signed short Appear_end; // size: 0x2, address: 0x578A98
+
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", appear_work_clear);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", home_visitor_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", appear_data_set);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", appear_data_init_set);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", appear_player);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_00000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_01000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_03000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_04000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_05000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_06000);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", appear_data);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_07000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_08000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", sean_appear_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_09000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_10000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_11000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_12000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_13000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_14000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_15000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_16000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", gill_appear_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_17000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_18000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_19000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_20000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_21000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_22000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_23000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_24000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_25000);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", smoke_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_26000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_28000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_29000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", animal_decide);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", don_appear_check);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_30000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_31000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_32000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_33000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_34000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_36000);
+
+INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", animal_decide_tbl);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_37000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_38000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_39000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", Appear_41000);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/appear", gouki_appear);
