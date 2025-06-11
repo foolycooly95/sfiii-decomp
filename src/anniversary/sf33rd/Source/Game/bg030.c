ff0ad65a3b0d53a9b6e5b77e442a31339d7bec8d
foolycooly95 <jackwoodman95@gmail.com>
2025-06-11 19:57:36 +0100

    Update bg030.c

--- src/anniversary/sf33rd/Source/Game/bg030.c
+++ src/anniversary/sf33rd/Source/Game/bg030.c
@@ -6,22 +6,40 @@
 #include "sf33rd/Source/Game/PLCNT.h"
 #include "sf33rd/Source/Game/WORK_SYS.h"
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg030", BG030);
-
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg030", bg0300);
+void BG030() {
+    bgw_ptr = &bg_w.bgw[1];
+    bg0301();
+    bgw_ptr = bg_w.bgw;
+    bg0300();
+    zoom_ud_check();
+    bg_pos_hosei2();
+    Bg_Family_Set();
+}
 
 void bg0300() {
   void (* bg0300_jmp[2])() = { bg0300_init00, bg_move_common };
   bg0300_jmp[bgw_ptr->r_no_0]();
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg030", bg0300_init00);
-
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg030", bg0301);
+void bg0300_init00() {
+    bgw_ptr->r_no_0++;
+    bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+    bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+    bgw_ptr->zuubun = 0;
+}
 
 void bg0301() {
   void (* bg0301_jmp[2])() = { bg0301_init00, bg_base_move_common };
   bg0301_jmp[bgw_ptr->r_no_0]();
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg030", bg0301_init00);
+void bg0301_init00() {
+    bgw_ptr->r_no_0++;
+    bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+    bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+    bgw_ptr->zuubun = 0;
+    effect_05_init();
+    effect_06_init();
+    effect_71_init();
+    effect_L2_init();
+}
