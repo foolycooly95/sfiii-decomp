a2efbaf09a4b07d3417caf775d96f156474f900d
foolycooly95 <jackwoodman95@gmail.com>
2025-06-12 13:04:11 +0100

    Update bg130.c

--- src/anniversary/sf33rd/Source/Game/bg130.c
+++ src/anniversary/sf33rd/Source/Game/bg130.c
@@ -6,24 +6,62 @@
 #include "sf33rd/Source/Game/PLCNT.h"
 #include "sf33rd/Source/Game/WORK_SYS.h"
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg130", BG130);
-
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg130", bg1301);
+void BG130() {
+    bgw_ptr = &bg_w.bgw[1];
+    bg1301();
+    bgw_ptr = bg_w.bgw;
+    bg1300();
+    bgw_ptr = &bg_w.bgw[2];
+    bg1302();
+    zoom_ud_check();
+    bg_pos_hosei2();
+    Bg_Family_Set();
+}
 
 void bg1301() {
   void (* bg1301_jmp[2])() = { bg1301_init00, bg_base_move_common };
   bg1301_jmp[bgw_ptr->r_no_0]();
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg130", bg1301_init00);
-
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg130", bg1300);
+void bg1301_init00() {
+    bgw_ptr->r_no_0++;
+    bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+    bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+    bgw_ptr->zuubun = 0;
+    effect_05_init();
+}
 
 void bg1300() {
   void (* bg1300_jmp[2])() = { bg1300_init00, bg_move_common };
   bg1300_jmp[bgw_ptr->r_no_0]();
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg130", bg1300_init00);
+void bg1300_init00() {
+    bgw_ptr->r_no_0++;
+    bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+    bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+    bgw_ptr->zuubun = 0;
+}
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg130", bg1302);
+void bg1302() {
+    switch (bgw_ptr->r_no_0) {
+    case 0:
+        bgw_ptr->r_no_0++;
+        bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+        bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+        bgw_ptr->zuubun = 0;
+        bgw_ptr->fam_no = 2;
+        bgw_ptr->xy[1].disp.pos = bgw_ptr->pos_y_work = 0;
+        bgw_ptr->y_limit = bgw_ptr->y_limit2 = 0xD0;
+        bgw_ptr->speed_x = 0xE000;
+        bgw_ptr->speed_y = 0xE000;
+        sync_fam_set3(2);
+        break;
+
+    case 1:
+        bg_x_move_check();
+        bg_y_move_check();
+        sync_fam_set3(2);
+        break;
+    }
+}
