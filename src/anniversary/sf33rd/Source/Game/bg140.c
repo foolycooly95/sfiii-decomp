122b430854d7cdbf95c60d8781dfc78403477168
foolycooly95 <jackwoodman95@gmail.com>
2025-06-12 13:49:59 +0100

    bg140 and bg150

--- src/anniversary/sf33rd/Source/Game/bg140.c
+++ src/anniversary/sf33rd/Source/Game/bg140.c
@@ -6,24 +6,61 @@
 #include "sf33rd/Source/Game/PLCNT.h"
 #include "sf33rd/Source/Game/WORK_SYS.h"
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg140", BG140);
-
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg140", bg1401);
+void BG140(void) {
+    bgw_ptr = &bg_w.bgw[1];
+    bg1401();
+    bgw_ptr = bg_w.bgw;
+    bg1400();
+    bgw_ptr = &bg_w.bgw[2];
+    bg1402();
+    zoom_ud_check();
+    bg_pos_hosei2();
+    Bg_Family_Set();
+}
 
 void bg1401() {
-  void (* bg1401_jmp[2])() = { bg1401_init00, bg_base_move_common };
-  bg1401_jmp[bgw_ptr->r_no_0]();
+    void (* bg1401_jmp[2])() = { bg1401_init00, bg_base_move_common };
+    bg1401_jmp[bgw_ptr->r_no_0]();
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg140", bg1401_init00);
-
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg140", bg1400);
+void bg1401_init00() {
+    bgw_ptr->r_no_0++;
+    bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+    bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+    bgw_ptr->zuubun = 0;
+    bg_app = 0;
+    effect_06_init();
+    effect_12_init(0);
+}
 
 void bg1400() {
-  void (* bg140_jmp[2])() = { bg1400_init00, bg_move_common };
-  bg140_jmp[bgw_ptr->r_no_0]();
+    void (* bg140_jmp[2])() = { bg1400_init00, bg_move_common };
+    bg140_jmp[bgw_ptr->r_no_0]();
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg140", bg1400_init00);
+void bg1400_init00() {
+    bgw_ptr->r_no_0++;
+    bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+    bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+    bgw_ptr->zuubun = 0;
+}
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg140", bg1402);
+void bg1402() {
+    switch (bgw_ptr->r_no_0) {
+    case 0:
+        bgw_ptr->r_no_0++;
+        bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+        bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+        bgw_ptr->xy[1].disp.pos = bgw_ptr->pos_y_work = 0;
+        bgw_ptr->fam_no = 2;
+        bgw_ptr->xy[0].disp.low = bgw_ptr->xy[1].disp.low = 0;
+        bgw_ptr->y_limit = bgw_ptr->y_limit2 = 0xF0;
+        bgw_ptr->speed_x = 0xA000;
+        bgw_ptr->speed_y = 0xE000;
+        break;
+
+    case 1:
+        bg_move_common();
+        break;
+    }
+}
--- src/anniversary/sf33rd/Source/Game/bg150.c
+++ src/anniversary/sf33rd/Source/Game/bg150.c
@@ -6,24 +6,49 @@
 #include "sf33rd/Source/Game/PLCNT.h"
 #include "sf33rd/Source/Game/WORK_SYS.h"
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg150", BG150);
-
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg150", bg1501);
+void BG150() {
+    bgw_ptr = &bg_w.bgw[1];
+    bg1502();
+    bgw_ptr = bg_w.bgw;
+    bg1501();
+    bgw_ptr = &bg_w.bgw[2];
+    bg1502_sync_common();
+    zoom_ud_check();
+    bg_pos_hosei2();
+    Bg_Family_Set();
+}
 
 void bg1501() {
   void (* bg1501_jmp[2])() = { bg1501_init00, bg_move_common };
   bg1501_jmp[bgw_ptr->r_no_0]();
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg150", bg1501_init00);
-
-// INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg150", bg1502);
+void bg1501_init00() {
+    bgw_ptr->r_no_0++;
+    bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+    bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+    bgw_ptr->zuubun = 0;
+}
 
 void bg1502() {
   void (* bg1502_jmp[2])() = { bg1502_init00, bg_base_move_common };
   bg1502_jmp[bgw_ptr->r_no_0]();
 }
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg150", bg1502_init00);
+void bg1502_init00() {
+    bgw_ptr->r_no_0++;
+    bgw_ptr->old_pos_x = bgw_ptr->xy[0].disp.pos = bgw_ptr->pos_x_work = 0x200;
+    bgw_ptr->hos_xy[0].cal = bgw_ptr->wxy[0].cal = bgw_ptr->xy[0].cal;
+    bgw_ptr->zuubun = 0;
+    effect_05_init();
+    effect_12_init(5);
+    effect_06_init();
+    effect_44_init(8);
+    effect_25_init(0);
+    effect_94_init(0);
+    effect_94_init(1);
+    effect_I4_init();
+    effect_85_init();
+}
 
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/bg150", bg1502_sync_common);
