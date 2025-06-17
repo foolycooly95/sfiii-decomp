7664d5412a5e0c7e4c7fe5b26d9fc8142b85cc8e
foolycooly95 <jackwoodman95@gmail.com>
2025-06-17 19:55:58 +0100

    effect_07_move & rodata

--- include/sf33rd/Source/Game/EFF07.h
+++ include/sf33rd/Source/Game/EFF07.h
@@ -3,6 +3,8 @@
 
 #include "types.h"
 
+extern const s16 eff07_data_tbl[2][3]; // size: 0xC, address: 0x506D60
+
 s32 effect_07_init();
 
 #endif
--- src/anniversary/sf33rd/Source/Game/EFF07.c
+++ src/anniversary/sf33rd/Source/Game/EFF07.c
@@ -1,6 +1,37 @@
 #include "common.h"
+#include "sf33rd/Source/Game/aboutspr.h"
+#include "sf33rd/Source/Game/CHARSET.h"
+#include "sf33rd/Source/Game/EFFECT.h"
+#include "sf33rd/Source/Game/SLOWF.h"
+#include "sf33rd/Source/Game/ta_sub.h"
+#include "sf33rd/Source/Game/workuser.h"
 
-INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF07", effect_07_move);
+void effect_07_move(WORK_Other* ewk) {
+    #if defined(TARGET_PS2)
+        void set_char_move_init(WORK * wk, s16 koc, s32 index);
+    #endif
+    if (obr_no_disp_check() == 0) {
+        switch (ewk->wu.routine_no[0]) {
+        case 0:
+            ewk->wu.routine_no[0]++;
+            ewk->wu.disp_flag = 1;
+            set_char_move_init(&ewk->wu, 0, ewk->wu.char_index);
+            break;
+
+        case 1:
+            if (!EXE_flag && !Game_pause && !EXE_obroll) {
+                char_move(&ewk->wu);
+            }
+            disp_pos_trans_entry_r(ewk);
+            break;
+
+        default:
+            all_cgps_put_back(&ewk->wu);
+            push_effect_work(&ewk->wu);
+            break;
+        }
+    }
+}
 
 #if defined(TARGET_PS2)
 INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF07", effect_07_init);
@@ -10,4 +41,6 @@ s32 effect_07_init() {
 }
 #endif
 
-INCLUDE_RODATA("asm/anniversary/nonmatchings/sf33rd/Source/Game/EFF07", eff07_data_tbl);
+const s16 eff07_data_tbl[2][3] = {
+    { 0x0004, 0x02e6, 0x001c }, { 0x0005, 0x0326, 0x001c}
+};
