2511d966c72b88b4b4c9747d422f1320c9a66c4e
foolycooly95 <jackwoodman95@gmail.com>
2025-06-17 14:59:00 +0100

    effL2

---
+++ include/sf33rd/Source/Game/effL2.h
@@ -0,0 +1,10 @@
+#ifndef EFFL2_H
+#define EFFL2_H
+
+#include "types.h"
+
+extern const s8 effl2_dir_tbl[2][16]; // size: 0x20, address: 0x51B3F0
+
+s32 effect_L2_init();
+
+#endif
---
+++ src/anniversary/sf33rd/Source/Game/effL2.c
@@ -0,0 +1,18 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/effL2", effect_L2_move);
+
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/effL2", effl2_dir_check);
+
+#if defined(TARGET_PS2)
+INCLUDE_ASM("asm/anniversary/nonmatchings/sf33rd/Source/Game/effL2", effect_L2_init);
+#else
+s32 effect_L2_init() {
+    not_implemented(__func__);
+}
+#endif
+
+const s8 effl2_dir_tbl[2][16] = {
+    { 0, 0, 0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 3 }
+};
