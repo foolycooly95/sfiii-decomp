bd77c23129c1407126feb0b1ee9eb4c101f77b72
foolycooly95 <jackwoodman95@gmail.com>
2025-06-20 00:26:23 +0100

    effL2

--- include/sf33rd/Source/Game/effL2.h
+++ include/sf33rd/Source/Game/effL2.h
@@ -1,10 +1,12 @@
 #ifndef EFFL2_H
 #define EFFL2_H
 
+#include "structs.h"
 #include "types.h"
 
 extern const s8 effl2_dir_tbl[2][16]; // size: 0x20, address: 0x51B3F0
 
+void effl2_dir_check(WORK_Other *ewk);
 s32 effect_L2_init();
 
 #endif
