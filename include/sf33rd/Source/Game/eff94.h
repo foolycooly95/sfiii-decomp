f39a82aa1d685a50494dd6ce8b084f4b14895b99
foolycooly95 <jackwoodman95@gmail.com>
2025-06-20 01:19:28 +0100

    eff94

--- include/sf33rd/Source/Game/eff94.h
+++ include/sf33rd/Source/Game/eff94.h
@@ -9,18 +9,18 @@ extern const s16 eff94_2000_tbl[8];     // size: 0x10, address: 0x50C140
 extern const s8 eff94_2000_1_tbl[16];   // size: 0x10, address: 0x50C150
 extern const s16 eff94_3000_tbl[4][3];  // size: 0x18, address: 0x50C160
 
-void eff94_0000();
-void eff94_1000();
-void eff94_2000();
-void eff94_2000_0();
-void eff94_2000_1();
-void eff94_2000_2();
-void eff94_2000_3();
-void eff94_2000_4();
-void eff94_3000();
-void eff94_3000_0();
-void eff94_3000_4();
-void eff94_4000();
+void eff94_0000(WORK_Other *ewk);
+void eff94_1000(WORK_Other *ewk);
+void eff94_2000(WORK_Other *ewk);
+void eff94_2000_0(WORK_Other *ewk);
+void eff94_2000_1(WORK_Other *ewk);
+void eff94_2000_2(WORK_Other *ewk);
+void eff94_2000_3(WORK_Other *ewk);
+void eff94_2000_4(WORK_Other *ewk);
+void eff94_3000(WORK_Other *ewk);
+void eff94_3000_0(WORK_Other *ewk);
+void eff94_3000_4(WORK_Other *ewk);
+void eff94_4000(WORK_Other *ewk);
 s32 effect_94_init(u8 type94);
 
 #endif
